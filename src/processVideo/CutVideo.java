f80f51cb1ca2f86efdeb6410399ead93d9845b27
Tuan Nguyen Van <38428343+nguyenvantuan2391996@users.noreply.github.com>
2019-06-18 17:57:15 +0700

    Add files via upload

---
+++ src/processVideo/CutVideo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package processVideo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nguyenvantuan
+ */
+public class CutVideo {
+
+    public String msg = "";
+
+    public String cutVideo() {
+        
+        Thread getByteVideo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                File file = new File("bin\\output.mp4");;
+                double sizeFile = 0d;
+
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        if (sizeFile == file.length()) {
+                            break;
+                        }
+                        System.out.println(file.length());
+                        sizeFile = file.length();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(CutVideo.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                msg = "Video was processed success";
+            }
+        });
+        getByteVideo.start();
+
+        // xử lý video
+        try {
+            Process p = Runtime.getRuntime().exec("cmd /c start hideCmd.vbs");
+
+            p.waitFor();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return msg;
+    }
+
+}
